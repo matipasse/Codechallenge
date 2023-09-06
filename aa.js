4d9ada4936de2504e0bcea6e5ad32f9222bb51ac
matipasse <matiaspasserini51@gmail.com>
2023-09-06 16:12:56 -0300

    creaciondearchivos

---
+++ aa.js
@@ -0,0 +1,13 @@
+let nombre = document.getElementById('nombre').value
+let apellido = document.getElementById('apellido').value
+let fechanacimiento = document.getElementById('fechanacimiento').value
+
+const info = {
+    nombre: nombre,
+    apellido: apellido,
+    fechadenacimiento: fechanacimiento
+
+}
+
+
+console.log(info)
